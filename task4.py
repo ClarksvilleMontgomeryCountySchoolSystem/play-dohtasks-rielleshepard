e5421ca76035fa63cb16b65f4fd3bbbacd4ad60d
rielleshepard <rielle.shepard058@students.cmcss.net>
2026-01-13 13:28:32 -0600

    Update task4.py

--- task4.py
+++ task4.py
@@ -1,1 +1,21 @@
-
+def main():
+    color1 = "yellow"
+    color2 = "red"
+    print(f"1) Roll a ball using {color1} for the body.\n")
+    choice1 = input("hot dog or round like a ball? ")
+    if choice1 == "hot dog":
+        print("2) Stretch it out.\n")
+    else:
+        print("2) Keep it round.\n")
+    print("3) Roll a smaller ball using {color1} for the head.\n")
+    print("4) Attach the head to the body.\n")
+    choice2 = input("standing up or flopped down? ")
+    if choice2 == "standing up":
+        print(f"5) Attach two pointed pieces using {color2} upright.\n")
+    else:
+        print(f"5) Attach two pieces using {color2} hanging downward.\n")
+    print(f"6) Add four legs using {color1}, a small tail using {color2}, two eyes, and a nose.\n")
+    print("7) Name this creation: \"Dog\"")
+    # TODO Conditional statement line 4
+    # TODO Add step
+    # TODO Add step
